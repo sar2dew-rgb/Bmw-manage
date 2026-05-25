212edc771c5e7f92302480bc54210898f9e3fb05
sar2dew-rgb <sar2dew@gmail.com>
2026-05-26 01:44:00 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,24 @@
 const CACHE_NAME = "bmw-app-v1";
-
-const urlsToCache = [
-  "./",
+const ASSETS = [
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "https://cdn-icons-png.flaticon.com/512/2785/2785819.png"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+// Install Service Worker and cache core assets
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+// Fetch assets from cache if offline
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
