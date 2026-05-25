68e91fe70926ab86e98708ec50c2707cc0bdcff2
sar2dew-rgb <sar2dew@gmail.com>
2026-05-25 11:06:52 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "bmw-waste-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "https://cdn-icons-png.flaticon.com/512/2966/2966487.png",
+  "https://cdn-icons-png.flaticon.com/512/3209/3209265.png",
+  "https://cdn-icons-png.flaticon.com/512/4320/4320371.png",
+  "https://cdn-icons-png.flaticon.com/512/6195/6195700.png",
+  "https://cdn-icons-png.flaticon.com/512/2785/2785819.png"
+];
+
+// Install Service Worker and Cache Assets
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate Service Worker and clear old caches
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch Offline Support
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
