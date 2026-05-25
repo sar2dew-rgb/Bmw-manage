f37a6ef2f1307b4e2cbf4c76eaa8c6bef3fbdfdd
sar2dew-rgb <sar2dew@gmail.com>
2026-05-26 01:26:15 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,23 @@
-const CACHE_NAME = "bmw-waste-v1";
-const ASSETS = [
+const CACHE_NAME = "bmw-app-v1";
+
+const urlsToCache = [
   "./",
   "./index.html",
-  "./manifest.json",
-  "file_00000000b2d471f891e27f78d95d5b81.png ",
-  "https://cdn-icons-png.flaticon.com/512/3209/3209265.png",
-  "https://cdn-icons-png.flaticon.com/512/4320/4320371.png",
-  "https://cdn-icons-png.flaticon.com/512/6195/6195700.png",
-  "https://cdn-icons-png.flaticon.com/512/2785/2785819.png"
+  "./manifest.json"
 ];
 
-// Install Service Worker and Cache Assets
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activate Service Worker and clear old caches
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Fetch Offline Support
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cachedResponse) => {
-      return cachedResponse || fetch(e.request);
-    })
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
